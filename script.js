d1a5e13041247a83d00e638680ab29190626a27a
Sraeburn987 <sraeburn98@gmail.com>
2026-02-08 21:51:06 +0000

    Add files via upload

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 import { createClient } from "https://esm.sh/@supabase/supabase-js@2";
 
-const SUPABASE_URL = "PASTE_YOUR_PROJECT_URL_HERE";
-const SUPABASE_ANON_KEY = "PASTE_YOUR_ANON_KEY_HERE";
+const SUPABASE_URL = "https://wmieslnlfrrwqvxhbdtn.supabase.co";
+const SUPABASE_ANON_KEY = "sb_publishable_l3Ha-qsAhqnK1QDeHaUYvw_ULYoCyAb";
 
 const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
 
